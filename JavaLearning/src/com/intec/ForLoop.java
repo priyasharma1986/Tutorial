df00166637747b6fc6c0430e826f60b94c62636d
priyasharma1986 <priyanka.sharma095@gmail.com>
2022-01-27 10:25:33 +0100

    forloop

---
+++ JavaLearning/src/com/intec/ForLoop.java
@@ -0,0 +1,16 @@
+package com.intec;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = -1;
+		
+		for(i=-1; i<30; i+=3) {
+			System.out.println(i);
+		}
+			
+         
+	}
+
+}
