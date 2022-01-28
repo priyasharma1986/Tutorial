b7b3bd58a3b3ba4d7e606996c0e2d01ed325b098
priyasharma1986 <priyanka.sharma095@gmail.com>
2022-01-28 09:53:58 +0100

    Method

---
+++ JavaLearning/src/com/intec/Method.java
@@ -0,0 +1,16 @@
+package com.intec;
+
+public class Method {
+	static void sumOfTwoNumbers(int n1, int n2) {
+		int getResult = n1 + n2;
+		System.out.println("sum of two numbers :" + getResult);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		sumOfTwoNumbers(45,45);
+		
+
+	}
+
+}
