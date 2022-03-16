4b1d88beadb5ef38db984a3168d3f192f650e6ba
priyasharma1986 <priyanka.sharma095@gmail.com>
2022-03-16 20:46:12 +0100

    practice

---
+++ JavaLearning/src/CodeWithHarry/Ex2.java
@@ -0,0 +1,17 @@
+package CodeWithHarry;
+
+public class Ex2 {
+
+	public static void main(String[] args) {
+
+		int a = 56;
+		int b = 68;
+		int c = 75;
+		
+		int sum = a + b + c;
+		
+		System.out.println("Sum of three numbers: "+ sum);
+				
+	}
+
+}
